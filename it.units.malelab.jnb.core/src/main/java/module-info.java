8b71a3e2b72d67ed99086340d34928c61a0b9c03
Eric Medvet <eric.medvet@gmail.com>
2022-11-22 17:06:28 +0100

    Modules added.

---
+++ it.units.malelab.jnb.core/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author "Eric Medvet" on 2022/08/28 for 2dmrsim
+ */
+module it.units.malelab.jnb.core {
+  requires java.logging;
+  exports it.units.malelab.jnb.core;
+}
---
+++ it.units.malelab.jnb.sample/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module it.units.malelab.jnb.sample {
+  requires it.units.malelab.jnb.core;
+}
