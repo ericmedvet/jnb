a7b0e4ef50c3a7efbc5f39399966b807bbd6398f
Eric Medvet <eric.medvet@gmail.com>
2023-03-22 18:26:59 +0100

    Preparing a "test" for default map addition.

--- io.github.ericmedvet.jnb.sample/src/main/java/io/github/ericmedvet/jnb/Main.java
+++ io.github.ericmedvet.jnb.sample/src/main/java/io/github/ericmedvet/jnb/Main.java
@@ -16,16 +16,16 @@ public class Main {
   public record Person(
       @Param(value = "", injection = Param.Injection.INDEX) int index,
       @Param("name") String name,
-      @Param("age") int age,
+      @Param(value = "age", dI = 44) int age,
       @Param("nicknames") List<String> nicknames
   ) {}
 
   private final static String S = """
       office(
-              head = person(name = "Mario Rossi"; age = 43);
+              head = person(name = "Mario Rossi");
               staff = + [
                 person(name = Alice; age = 33; nicknames = [Puce; "The Cice"]);
-                person(name = Bob; age = 25);
+                person(name = Bob);
                 person(name = Charlie; age = 38)
               ] + [person(name = Dane; age = 28)];
               roomNumbers = [202:1:205]
