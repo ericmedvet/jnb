2726a3baf288b8d4f846b86e9b04008f7ebf8026
Eric Medvet <eric.medvet@gmail.com>
2023-04-25 18:58:27 +0200

    Bug fixed in markdown doc creation concerning angle brackets.

--- io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/InfoPrinter.java
+++ io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/InfoPrinter.java
@@ -28,8 +28,7 @@ import java.util.stream.Collectors;
 public class InfoPrinter {
 
   private static final int N_OF_COMPATIBILITY_ATTEMPTS = 10;
-  private static final Set<DocumentedBuilder.Type> EASY_TYPES = EnumSet.of(
-      DocumentedBuilder.Type.BOOLEAN,
+  private static final Set<DocumentedBuilder.Type> EASY_TYPES = EnumSet.of(DocumentedBuilder.Type.BOOLEAN,
       DocumentedBuilder.Type.DOUBLE,
       DocumentedBuilder.Type.INT,
       DocumentedBuilder.Type.STRING
@@ -44,10 +43,7 @@ public class InfoPrinter {
   private final boolean computeCompatibilities;
 
   public InfoPrinter(
-      int packageHeadingLevel,
-      int builderHeadingLevel,
-      boolean shortenFQNs,
-      boolean computeCompatibilities
+      int packageHeadingLevel, int builderHeadingLevel, boolean shortenFQNs, boolean computeCompatibilities
   ) {
     this.packageHeadingLevel = packageHeadingLevel;
     this.builderHeadingLevel = builderHeadingLevel;
@@ -59,20 +55,14 @@ public class InfoPrinter {
     this(PACKAGE_HEADING_LEVEL, BUILDER_HEADING_LEVEL, true, false);
   }
 
-  record BuilderInfo(
-      SortedSet<Name> names, Builder<?> builder, List<ParamCompatibility> compatibilities,
-      SortedSet<String> workingUsages
-  ) {
+  record BuilderInfo(SortedSet<Name> names, Builder<?> builder, List<ParamCompatibility> compatibilities,
+                     SortedSet<String> workingUsages) {
     public Name longestName() {
-      return names().stream()
-          .max(Comparator.comparingInt(n -> n.fullName().length()))
-          .orElseThrow();
+      return names().stream().max(Comparator.comparingInt(n -> n.fullName().length())).orElseThrow();
     }
 
     public Name shortestName() {
-      return names().stream()
-          .min(Comparator.comparingInt(n -> n.fullName().length()))
-          .orElseThrow();
+      return names().stream().min(Comparator.comparingInt(n -> n.fullName().length())).orElseThrow();
     }
   }
 
@@ -125,17 +115,13 @@ public class InfoPrinter {
     return builder.stream()
         .filter(pi -> pi.defaultValue() == null)
         .filter(pi -> EASY_TYPES.contains(pi.type()))
-        .map(pi -> new ParamTriplet(
-            pi.name(),
-            switch (pi.type()) {
-              case INT -> "1";
-              case DOUBLE -> "1.0";
-              case STRING -> "a";
-              case BOOLEAN -> "false";
-              default -> throw new IllegalStateException("Unexpected value: " + pi.type());
-            },
-            null
-        ))
+        .map(pi -> new ParamTriplet(pi.name(), switch (pi.type()) {
+          case INT -> "1";
+          case DOUBLE -> "1.0";
+          case STRING -> "a";
+          case BOOLEAN -> "false";
+          default -> throw new IllegalStateException("Unexpected value: " + pi.type());
+        }, null))
         .toList();
   }
 
@@ -147,7 +133,8 @@ public class InfoPrinter {
     List<List<ParamTriplet>> paramPairs = new ArrayList<>();
     //easy case
     paramPairs.add(easyParamPairs(builder.params()));
-    List<DocumentedBuilder.ParamInfo> npmParams = builder.params().stream()
+    List<DocumentedBuilder.ParamInfo> npmParams = builder.params()
+        .stream()
         .filter(pi -> pi.type().equals(DocumentedBuilder.Type.NAMED_PARAM_MAP))
         .toList();
     if (!builderInfos.isEmpty() && !npmParams.isEmpty()) {
@@ -157,8 +144,7 @@ public class InfoPrinter {
       while (!done) {
         List<ParamTriplet> localParamTriplets = new ArrayList<>(easyParamPairs(builder.params()));
         for (int i = 0; i < indexes.length; i++) {
-          localParamTriplets.add(new ParamTriplet(
-              npmParams.get(i).name(),
+          localParamTriplets.add(new ParamTriplet(npmParams.get(i).name(),
               builderInfos.get(indexes[i]).workingUsages().first(),
               builderInfos.get(indexes[i])
           ));
@@ -187,7 +173,8 @@ public class InfoPrinter {
     Map<Name, BuilderInfo> aliasesMap = new HashMap<>();
     @SuppressWarnings("unchecked") Map<String, Builder<?>> builders = (Map<String, Builder<?>>) nb.getBuilders();
     builders.forEach((fullName, builder) -> {
-      Name mainName = aliasesMap.entrySet().stream()
+      Name mainName = aliasesMap.entrySet()
+          .stream()
           .filter(e -> e.getValue().builder() == builder)
           .findFirst()
           .map(Map.Entry::getKey)
@@ -195,16 +182,15 @@ public class InfoPrinter {
       if (mainName == null) {
         List<ParamCompatibility> compatibilities = new ArrayList<>();
         if (builder instanceof DocumentedBuilder<?> documentedBuilder) {
-          documentedBuilder.params().stream()
+          documentedBuilder.params()
+              .stream()
               .filter(pi -> pi.injection().equals(Param.Injection.NONE))
               .forEach(pi -> compatibilities.add(new ParamCompatibility(pi, new ArrayList<>())));
         }
-        aliasesMap.put(new Name(fullName), new BuilderInfo(
-            new TreeSet<>(),
-            builder,
-            Collections.unmodifiableList(compatibilities),
-            new TreeSet<>()
-        ));
+        aliasesMap.put(
+            new Name(fullName),
+            new BuilderInfo(new TreeSet<>(), builder, Collections.unmodifiableList(compatibilities), new TreeSet<>())
+        );
         mainName = new Name(fullName);
       }
       aliasesMap.get(mainName).names.add(new Name(fullName));
@@ -213,11 +199,11 @@ public class InfoPrinter {
       //find usages
       List<BuilderInfo> buildableBuilders = new ArrayList<>();
       int nOfAttempts = 0;
-      while (aliasesMap.values().stream().anyMatch(bi -> bi.workingUsages()
-          .isEmpty()) && nOfAttempts < N_OF_COMPATIBILITY_ATTEMPTS) {
+      while (aliasesMap.values()
+          .stream()
+          .anyMatch(bi -> bi.workingUsages().isEmpty()) && nOfAttempts < N_OF_COMPATIBILITY_ATTEMPTS) {
         nOfAttempts = nOfAttempts + 1;
-        System.out.printf(
-            "Usages attempt %d on %d: %d known, %d unknown%n",
+        System.out.printf("Usages attempt %d on %d: %d known, %d unknown%n",
             nOfAttempts,
             N_OF_COMPATIBILITY_ATTEMPTS,
             buildableBuilders.size(),
@@ -227,15 +213,10 @@ public class InfoPrinter {
           if (bi.workingUsages().isEmpty()) {
             if (bi.builder() instanceof DocumentedBuilder<?> builder) {
               List<List<ParamTriplet>> triplets = paramTriplets(builder, buildableBuilders);
-              System.out.printf(
-                  "Usages for %s with %d cases%n",
-                  bi.shortestName().fullName(),
-                  triplets.size()
-              );
+              System.out.printf("Usages for %s with %d cases%n", bi.shortestName().fullName(), triplets.size());
               for (List<ParamTriplet> paramTriplets : triplets) {
                 try {
-                  String usage = "%s(%s)".formatted(
-                      bi.shortestName().fullName(),
+                  String usage = "%s(%s)".formatted(bi.shortestName().fullName(),
                       paramTriplets.stream()
                           .map(pp -> "%s=%s".formatted(pp.name(), pp.value()))
                           .collect(Collectors.joining(";"))
@@ -259,21 +240,20 @@ public class InfoPrinter {
             continue;
           }
           List<List<ParamTriplet>> triplets = paramTriplets(builder, buildableBuilders);
-          System.out.printf(
-              "Compatibility for %s with %d cases%n",
+          System.out.printf("Compatibility for %s with %d cases%n",
               builderInfo.shortestName().fullName(),
               triplets.size()
           );
           for (List<ParamTriplet> paramTriplets : triplets) {
             try {
-              String usage = "%s(%s)".formatted(
-                  builderInfo.shortestName().fullName(),
+              String usage = "%s(%s)".formatted(builderInfo.shortestName().fullName(),
                   paramTriplets.stream()
                       .map(pt -> "%s=%s".formatted(pt.name(), pt.value()))
                       .collect(Collectors.joining(";"))
               );
               nb.build(usage);
-              builderInfo.compatibilities().stream()
+              builderInfo.compatibilities()
+                  .stream()
                   .filter(c -> c.paramInfo().type().equals(DocumentedBuilder.Type.NAMED_PARAM_MAP))
                   .filter(c -> c.paramInfo().defaultValue() == null)
                   .forEach(c -> {
@@ -296,12 +276,12 @@ public class InfoPrinter {
     //group all by packages
     Map<String, PackageInfo> packagesMap = new HashMap<>();
     aliasesMap.forEach((name, builderInfo) -> {
-      String longestPackageName = builderInfo.names().stream()
+      String longestPackageName = builderInfo.names()
+          .stream()
           .map(Name::packageName)
           .max(Comparator.comparingInt(String::length))
           .orElseThrow();
-      PackageInfo packageInfo = packagesMap.getOrDefault(
-          longestPackageName,
+      PackageInfo packageInfo = packagesMap.getOrDefault(longestPackageName,
           new PackageInfo(new TreeSet<>(), new ArrayList<>())
       );
       packageInfo.names().addAll(builderInfo.names().stream().map(Name::packageName).toList());
@@ -323,7 +303,8 @@ public class InfoPrinter {
         ps.println(packageInfo.names().stream().map("`%s`"::formatted).collect(Collectors.joining(", ")));
         ps.println();
       }
-      packageInfo.builderInfos().stream()
+      packageInfo.builderInfos()
+          .stream()
           .sorted(Comparator.comparing(builderInfo -> builderInfo.shortestName().simpleName()))
           .forEach(builderInfo -> {
             ps.printf("%s Builder `%s()`%n", heading(builderHeadingLevel), builderInfo.longestName().fullName());
@@ -334,10 +315,10 @@ public class InfoPrinter {
                 ps.printf("`%s()`%n", builderInfo.shortestName().fullName());
               } else {
                 //signature
-                ps.printf(
-                    "`%s(%s)`%n",
+                ps.printf("`%s(%s)`%n",
                     builderInfo.shortestName().fullName(),
-                    documentedBuilder.params().stream()
+                    documentedBuilder.params()
+                        .stream()
                         .filter(p -> p.injection().equals(Param.Injection.NONE))
                         .map(DocumentedBuilder.ParamInfo::name)
                         .collect(Collectors.joining("; "))
@@ -353,14 +334,14 @@ public class InfoPrinter {
                 }
                 for (ParamCompatibility compatibility : builderInfo.compatibilities()) {
                   if (compatibility.paramInfo().injection().equals(Param.Injection.NONE)) {
-                    ps.printf(
-                        "| `%s` | %s | %s | %s |" + (computeCompatibilities ? " %s|" : "") + "%n",
+                    ps.printf("| `%s` | %s | %s | %s |" + (computeCompatibilities ? " %s|" : "") + "%n",
                         compatibility.paramInfo().name(),
                         compatibility.paramInfo().type().rendered(),
                         Objects.isNull(compatibility.paramInfo()
                             .defaultValue()) ? "" : "`%s`".formatted(compatibility.paramInfo().defaultValue()),
                         shortenJavaTypeName(compatibility.paramInfo().javaType()),
-                        compatibility.builderInfos().stream()
+                        compatibility.builderInfos()
+                            .stream()
                             .sorted(Comparator.comparing(bi -> bi.shortestName().fullName()))
                             .map(bi -> "`%s()`".formatted(bi.shortestName().fullName()))
                             .collect(Collectors.joining("<br>"))
@@ -380,11 +361,11 @@ public class InfoPrinter {
   }
 
   private String shortenJavaTypeName(Type javaType) {
+    String typeName = javaType.getTypeName().replace("<", "&lt;").replace(">", "&gt;");
     if (!shortenFQNs) {
-      return "`%s`".formatted(javaType.getTypeName());
+      return "`%s`".formatted(typeName);
     }
-    String longName = javaType.getTypeName();
-    String abbrName = longName.replaceAll("([a-z][a-zA-Z0-9_.]+)\\.([a-zA-Z0-9$]+)", "<abbr title=\"$0\">$2</abbr>");
+    String abbrName = typeName.replaceAll("([a-z][a-zA-Z0-9_.]+)\\.([a-zA-Z0-9$]+)", "<abbr title=\"$0\">$2</abbr>");
     return "<code>%s</code>".formatted(abbrName);
   }
 }
