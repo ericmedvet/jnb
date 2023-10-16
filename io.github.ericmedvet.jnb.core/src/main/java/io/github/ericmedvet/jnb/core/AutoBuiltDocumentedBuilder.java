9eba3f1e9541b330e164f159b79d8b027680a145
Eric Medvet <eric.medvet@gmail.com>
2023-10-16 16:32:07 +0200

    All rebuilt.

--- io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/AutoBuiltDocumentedBuilder.java
+++ io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/AutoBuiltDocumentedBuilder.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,7 @@ import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 public record AutoBuiltDocumentedBuilder<T>(
     String name, java.lang.reflect.Type builtType, List<ParamInfo> params, Builder<T> builder)
     implements DocumentedBuilder<T> {
--- io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/MapNamedParamMap.java
+++ io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/MapNamedParamMap.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 public class MapNamedParamMap implements NamedParamMap, Formattable {
 
   public record TypedKey(String name, Type type) implements Comparable<TypedKey> {
