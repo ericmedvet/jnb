5a46d9b164315c048f37b570714f0c9af4c7c1d7
Eric Medvet <eric.medvet@gmail.com>
2024-03-09 17:50:03 +0100

    Fixed: miscs name.

--- io.github.ericmedvet.jnb.buildable/src/main/java/io/github/ericmedvet/jnb/buildable/Miscs.java
+++ io.github.ericmedvet.jnb.buildable/src/main/java/io/github/ericmedvet/jnb/buildable/Miscs.java
@@ -1,17 +1,36 @@
+/*-
+ * ========================LICENSE_START=================================
+ * jnb-buildable
+ * %%
+ * Copyright (C) 2023 - 2024 Eric Medvet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 package io.github.ericmedvet.jnb.buildable;
 
+import io.github.ericmedvet.jnb.core.Discoverable;
 import io.github.ericmedvet.jnb.core.Param;
 import io.github.ericmedvet.jnb.datastructure.DoubleRange;
 import io.github.ericmedvet.jnb.datastructure.Grid;
-
 import java.util.List;
 import java.util.Random;
 import java.util.random.RandomGenerator;
 
+@Discoverable(prefixTemplate = "misc|m")
 public class Miscs {
 
-  private Miscs() {
-  }
+  private Miscs() {}
 
   @SuppressWarnings("unused")
   public static RandomGenerator defaultRG(@Param(value = "seed", dI = 0) int seed) {
@@ -31,5 +50,4 @@ public class Miscs {
   public static DoubleRange range(@Param("min") double min, @Param("max") double max) {
     return new DoubleRange(min, max);
   }
-
 }
