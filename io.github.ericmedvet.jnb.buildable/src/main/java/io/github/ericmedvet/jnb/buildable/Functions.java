b41a7327b6e2b481232883c1a48e81240e58c9bc
Eric Medvet <eric.medvet@gmail.com>
2024-03-09 01:28:32 +0100

    Added: unused annotations.

--- io.github.ericmedvet.jnb.buildable/src/main/java/io/github/ericmedvet/jnb/buildable/Functions.java
+++ io.github.ericmedvet.jnb.buildable/src/main/java/io/github/ericmedvet/jnb/buildable/Functions.java
@@ -23,7 +23,6 @@ import io.github.ericmedvet.jnb.core.Discoverable;
 import io.github.ericmedvet.jnb.core.Param;
 import io.github.ericmedvet.jnb.datastructure.FormattedNamedFunction;
 import io.github.ericmedvet.jnb.datastructure.NamedFunction;
-
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.Base64;
@@ -38,25 +37,24 @@ public class Functions {
 
   private static final Logger L = Logger.getLogger(Functions.class.getName());
 
-  private Functions() {
-  }
+  private Functions() {}
 
+  @SuppressWarnings("unused")
   public static <X> FormattedNamedFunction<X, Double> avg(
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, List<? extends Number>> beforeF,
-      @Param(value = "format", dS = "%.1f") String format
-  ) {
+      @Param(value = "format", dS = "%.1f") String format) {
     Function<List<? extends Number>, Double> f =
         vs -> vs.stream().mapToDouble(Number::doubleValue).average().orElseThrow();
     return FormattedNamedFunction.from(f, format, "avg").compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X> NamedFunction<X, String> toBase64(
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Object> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<Object, String> f = x -> {
       try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
-           ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+          ObjectOutputStream oos = new ObjectOutputStream(baos)) {
         oos.writeObject(x);
         oos.flush();
         return Base64.getEncoder().encodeToString(baos.toByteArray());
@@ -68,104 +66,118 @@ public class Functions {
     return FormattedNamedFunction.from(f, format, "to.base64").compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X> Function<X, X> identity() {
     return x -> x;
   }
 
+  @SuppressWarnings("unused")
   public static <X, C extends Comparable<C>> FormattedNamedFunction<X, C> max(
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<C>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<Collection<C>, C> f =
         cs -> cs.stream().max(Comparable::compareTo).orElseThrow();
     return FormattedNamedFunction.from(f, format, "max").compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X, C extends Comparable<C>> FormattedNamedFunction<X, C> min(
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<C>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<Collection<C>, C> f =
         cs -> cs.stream().min(Comparable::compareTo).orElseThrow();
     return FormattedNamedFunction.from(f, format, "min").compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X, C extends Comparable<C>> FormattedNamedFunction<X, C> percentile(
       @Param("p") double p,
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<C>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<Collection<C>, C> f = cs ->
         cs.stream().sorted().toList().get((int) Math.min(cs.size() - 1, Math.max(0, cs.size() * p / 100d)));
-    return FormattedNamedFunction.from(f, format, "percentile[%02.0f]".formatted(p)).compose(beforeF);
+    return FormattedNamedFunction.from(f, format, "percentile[%02.0f]".formatted(p))
+        .compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X, C extends Comparable<C>> FormattedNamedFunction<X, C> median(
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<C>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<Collection<C>, C> f =
         cs -> cs.stream().sorted().toList().get(Math.min(cs.size() - 1, Math.max(0, cs.size() / 2)));
     return FormattedNamedFunction.from(f, format, "median").compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X, T, R> NamedFunction<X, Collection<R>> each(
       @Param("mapF") Function<T, R> mapF,
-      @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<T>> beforeF
-  ) {
+      @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<T>> beforeF) {
     Function<Collection<T>, Collection<R>> f = ts -> ts.stream().map(mapF).toList();
-    return NamedFunction.from(f, "each[%s]".formatted(NamedFunction.name(mapF))).compose(beforeF);
+    return NamedFunction.from(f, "each[%s]".formatted(NamedFunction.name(mapF)))
+        .compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X, T> FormattedNamedFunction<X, T> nTh(
       @Param("n") int n,
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, List<T>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<List<T>, T> f = ts -> ts.get(n);
     return FormattedNamedFunction.from(f, format, "[%d]".formatted(n)).compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X, T> FormattedNamedFunction<X, List<T>> nkTh(
       @Param("n") int n,
       @Param("k") int k,
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, List<T>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<List<T>, List<T>> f = ts -> IntStream.range(0, ts.size())
         .filter(i -> (i % n) == k)
         .mapToObj(ts::get)
         .toList();
-    return FormattedNamedFunction.from(f, format, "[%di+%d]".formatted(n, k)).compose(beforeF);
+    return FormattedNamedFunction.from(f, format, "[%di+%d]".formatted(n, k))
+        .compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X, T> FormattedNamedFunction<X, List<T>> subList(
       @Param("from") double from,
       @Param("to") double to,
       @Param(value = "relative", dB = true) boolean relative,
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, List<T>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<List<T>, List<T>> f =
         ts -> ts.subList((int) Math.min(Math.max(0, relative ? (from * ts.size()) : from), ts.size()), (int)
             Math.min(Math.max(0, relative ? (to * ts.size()) : to), ts.size()));
-    return FormattedNamedFunction.from(f, format, "sub[%s%f-%f]".formatted(relative ? "%" : "", from, to)).compose(beforeF);
+    return FormattedNamedFunction.from(f, format, "sub[%s%f-%f]".formatted(relative ? "%" : "", from, to))
+        .compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X> FormattedNamedFunction<X, Double> quantized(
       @Param("q") double q,
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Number> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<Number, Double> f = v -> q * Math.floor(v.doubleValue() / q + 0.5);
     return FormattedNamedFunction.from(f, format, "q[%.1f]".formatted(q)).compose(beforeF);
   }
 
+  @SuppressWarnings("unused")
   public static <X> FormattedNamedFunction<X, Integer> size(
       @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<?>> beforeF,
-      @Param(value = "format", dS = "%s") String format
-  ) {
+      @Param(value = "format", dS = "%s") String format) {
     Function<Collection<?>, Integer> f = Collection::size;
     return FormattedNamedFunction.from(f, format, "size").compose(beforeF);
+  }
+
+  @SuppressWarnings("unused")
+  public static <X> FormattedNamedFunction<X, Double> uniqueness(
+      @Param(value = "beforeF", dNPM = "f.identity()") Function<X, Collection<?>> beforeF,
+      @Param(value = "format", dS = "%5.3f") String format) {
+    Function<Collection<?>, Double> f =
+        ts -> (double) ts.stream().distinct().count() / (double) ts.size();
+    return FormattedNamedFunction.from(f, format, "uniqueness").compose(beforeF);
   }
 }
