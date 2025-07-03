fead7f912b97214c24f9dfec76c5f985acaf616f
Eric Medvet <eric.medvet@gmail.com>
2025-07-03 23:51:56 +0200

    Improved: javadoc for Builder.cached().

--- io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/Builder.java
+++ io.github.ericmedvet.jnb.core/src/main/java/io/github/ericmedvet/jnb/core/Builder.java
@@ -27,36 +27,39 @@ import java.util.WeakHashMap;
 /// @param <T> the type of the object to build
 @FunctionalInterface
 public interface Builder<T> {
-  /// Builds an object of type `T` from a `ParamMap` and a `NamedBuilder`.
-  /// This method calls the overloaded `build` method with an index of 0.
+
+  /// Builds an object of type `T` from a `ParamMap` and a `NamedBuilder`. This method calls the
+  /// overloaded `build` method with an index of 0.
   ///
   /// @param map          the `ParamMap` containing the parameters for building the object
-  /// @param namedBuilder the `NamedBuilder` that can be used to recursively build values in the `ParamMap`
+  /// @param namedBuilder the `NamedBuilder` that can be used to recursively build values in the
+  ///                     `ParamMap`
   /// @return the built object
   /// @throws BuilderException if an error occurs during the building process
   default T build(ParamMap map, NamedBuilder<?> namedBuilder) throws BuilderException {
     return build(map, namedBuilder, 0);
   }
 
-  /// Builds an object of type `T` from a `ParamMap`, a `NamedBuilder`, and an index.
-  /// The `ParamMap` is expected to contain the information needed to build the object, as name-value pairs.
-  /// The `NamedBuilder` is used to recursively build objects when the values in the `ParamMap` are of a type
-  /// different from [io.github.ericmedvet.jnb.core.ParamMap.Type#NAMED_PARAM_MAP] and from
-  /// [io.github.ericmedvet.jnb.core.ParamMap.Type#NAMED_PARAM_MAPS].
-  /// The `index` is available when building sequences and is expected to be set accordingly by the caller.
+  /// Builds an object of type `T` from a `ParamMap`, a `NamedBuilder`, and an index. The `ParamMap`
+  /// is expected to contain the information needed to build the object, as name-value pairs. The
+  /// `NamedBuilder` is used to recursively build objects when the values in the `ParamMap` are of a
+  /// type different from [io.github.ericmedvet.jnb.core.ParamMap.Type#NAMED_PARAM_MAP] and from
+  /// [io.github.ericmedvet.jnb.core.ParamMap.Type#NAMED_PARAM_MAPS]. The `index` is available when
+  /// building sequences and is expected to be set accordingly by the caller.
   ///
   /// @param map          the `ParamMap` containing the parameters for building the object
-  /// @param namedBuilder the `NamedBuilder` that can be used to recursively build values in the `ParamMap`
-  /// @param index        the index of the object to build (different from 0 if the object is a part of sequence)
+  /// @param namedBuilder the `NamedBuilder` that can be used to recursively build values in the
+  ///                     `ParamMap`
+  /// @param index        the index of the object to build (different from 0 if the object is a part
+  ///                     of sequence)
   /// @return the built object
   /// @throws BuilderException if an error occurs during the building process
   T build(ParamMap map, NamedBuilder<?> namedBuilder, int index) throws BuilderException;
 
-  /// Returns a cached version of this `Builder`.
-  /// The cached builder stores previously built objects in a cache,
-  /// so that if the same `ParamMap` is provided again, the cached object is returned
-  /// instead of building a new one.
-  /// Internally, the cache is a `WeakHashMap`; the index is ignored when checking for the presence of a map in the cache.
+  /// Returns a cached version of this `Builder`. The cached builder stores previously built objects
+  /// in a cache, so that if the same `ParamMap` is provided again, the cached object is returned
+  /// instead of building a new one. Internally, the cache is a `WeakHashMap`; the index is ignored
+  /// when checking for the presence of a map in the cache.
   ///
   /// @return the cached version of this builder
   default Builder<T> cached() {
