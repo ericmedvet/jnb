221cdbd540420eda11bba6e0c280e701c45ac9da
Eric Medvet <eric.medvet@gmail.com>
2025-11-22 17:02:51 +0100

    feat: take the Composed interface from jsdynsym

---
+++ io.github.ericmedvet.jnb.datastructure/src/main/java/io/github/ericmedvet/jnb/datastructure/Composed.java
@@ -0,0 +1,91 @@
+/*-
+ * ========================LICENSE_START=================================
+ * jnb-datastructure
+ * %%
+ * Copyright (C) 2023 - 2025 Eric Medvet
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+package io.github.ericmedvet.jnb.datastructure;
+
+import java.util.Optional;
+
+public interface Composed<C> {
+  C inner();
+
+  static <K> Optional<K> deepest(Object o, Class<K> kClass) {
+    if (o instanceof Composed<?> c) {
+      return c.deepest(kClass);
+    }
+    if (kClass.isAssignableFrom(o.getClass())) {
+      //noinspection unchecked
+      return Optional.of((K) o);
+    }
+    return Optional.empty();
+  }
+
+  static <K> Optional<K> shallowest(Object o, Class<K> kClass) {
+    if (kClass.isAssignableFrom(o.getClass())) {
+      //noinspection unchecked
+      return Optional.of((K) o);
+    }
+    if (o instanceof Composed<?> c) {
+      return c.shallowest(kClass);
+    }
+    return Optional.empty();
+  }
+
+  default Object deepest() {
+    if (inner() instanceof Composed<?> composed) {
+      return composed.deepest();
+    }
+    return inner();
+  }
+
+  default <K> Optional<K> deepest(Class<K> kClass) {
+    if (inner() instanceof Composed<?> composed) {
+      Optional<K> inside = composed.deepest(kClass);
+      if (inside.isPresent()) {
+        return inside;
+      }
+    }
+    if (kClass.isAssignableFrom(inner().getClass())) {
+      //noinspection unchecked
+      return Optional.of((K) inner());
+    }
+    if (kClass.isAssignableFrom(getClass())) {
+      //noinspection unchecked
+      return Optional.of((K) this);
+    }
+    return Optional.empty();
+  }
+
+  default <K> Optional<K> shallowest(Class<K> kClass) {
+    if (kClass.isAssignableFrom(getClass())) {
+      //noinspection unchecked
+      return Optional.of((K) this);
+    }
+    if (kClass.isAssignableFrom(inner().getClass())) {
+      //noinspection unchecked
+      return Optional.of((K) inner());
+    }
+    if (inner() instanceof Composed<?> composed) {
+      Optional<K> inside = composed.shallowest(kClass);
+      if (inside.isPresent()) {
+        return inside;
+      }
+    }
+    return Optional.empty();
+  }
+}
