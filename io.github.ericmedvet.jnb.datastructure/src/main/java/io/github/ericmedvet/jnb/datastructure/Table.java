a57db0dd1bdc20f054b5957de7f98973d333531f
Eric Medvet <eric.medvet@gmail.com>
2025-12-01 11:27:16 +0100

    fix: fix formatting error in docs

--- io.github.ericmedvet.jnb.datastructure/src/main/java/io/github/ericmedvet/jnb/datastructure/Table.java
+++ io.github.ericmedvet.jnb.datastructure/src/main/java/io/github/ericmedvet/jnb/datastructure/Table.java
@@ -31,10 +31,10 @@ import java.util.stream.Stream;
 /// indexed through pairs of coordinates `R`,`C`. `R` is the type of row indexes, `C` is the type of
 /// column indexes. Rows and columns have a well-defined encounter order.
 ///
-/// This interface provides methods for modifying the content of the cells ([#set(Object, Object,
-/// Object)]) or the content and the structure ([#clear()], [#addRow(Series)], [#addColumn(Series)],
-/// [#removeRow(Object)], [#removeColumn(Object)]). It also provides methods for obtaining views of
-/// (parts) of the table.
+/// This interface provides methods for modifying the content of the cells
+/// ([#set(Object, Object, Object)] or the content and the structure
+/// ([#clear()], [#addRow(Series)], [#addColumn(Series)], [#removeRow(Object)], [#removeColumn(Object)]).
+/// It also provides methods for obtaining views of (parts) of the table.
 ///
 /// For creating an unmodifiable table from data, one can use the `from` methods:
 /// [#from(SequencedMap)], [#fromRows(List)], and [#fromColumns(List)].
@@ -1098,7 +1098,7 @@ public interface Table<R, C, T> {
     }
   }
 
-  /// A series of values of tells, representing either a row or a column. If used to represent a
+  /// A series of values of cells, representing either a row or a column. If used to represent a
   /// row, the `primaryIndex` is the row index; otherwise, it is the column index.
   ///
   /// @param primaryIndex the row/column index of the series
