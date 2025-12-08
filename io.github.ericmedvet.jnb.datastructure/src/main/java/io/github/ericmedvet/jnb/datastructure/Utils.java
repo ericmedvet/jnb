aa16929433a55b68bb7c9ab60c5e4560cc20021d
Eric Medvet <eric.medvet@gmail.com>
2025-12-08 19:50:54 +0100

    docs: add javadoc for Utils

--- io.github.ericmedvet.jnb.datastructure/src/main/java/io/github/ericmedvet/jnb/datastructure/Utils.java
+++ io.github.ericmedvet.jnb.datastructure/src/main/java/io/github/ericmedvet/jnb/datastructure/Utils.java
@@ -48,6 +48,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import javax.imageio.ImageIO;
 
+/// This class consist of various `static` utility methods.
 public class Utils {
 
   private static final Logger L = Logger.getLogger(Utils.class.getName());
@@ -55,20 +56,45 @@ public class Utils {
   private Utils() {
   }
 
-  public static <T, R> Function<T, R> cached(Function<T, R> f) {
+  /// Returns a function that caches the results of the given `function`. Internally uses a
+  /// `WeakHashMap`.
+  ///
+  /// @param function the function to be cached
+  /// @param <T>      the type of the input to the function
+  /// @param <R>      the type of the result of the function
+  /// @return a new function that returns cached results if available, otherwise computes and caches
+  /// them
+  public static <T, R> Function<T, R> cached(Function<T, R> function) {
     Map<T, R> cache = new WeakHashMap<>();
-    return t -> cache.computeIfAbsent(t, f);
+    return t -> cache.computeIfAbsent(t, function);
   }
 
+  /// Concatenates the items of the provided `lists`.
+  ///
+  /// @param lists a list of lists of items to concatenate
+  /// @param <K>   the type of the items in the lists
+  /// @return an unmodified lists containing of the items of the provided lists
   public static <K> List<K> concat(List<List<? extends K>> lists) {
     return lists.stream().flatMap(List::stream).collect(Collectors.toList());
   }
 
+  /// Concatenates the items of the provided `lists`.
+  ///
+  /// @param lists a list of lists of items to concatenate
+  /// @param <K>   the type of the items in the lists
+  /// @return an unmodified lists containing of the items of the provided lists
   @SafeVarargs
   public static <K> List<K> concat(List<K>... lists) {
     return Arrays.stream(lists).flatMap(List::stream).collect(Collectors.toList());
   }
 
+  /// Attempts to execute the provided `runnable` and log if the execution fails.
+  ///
+  /// @param runnable        the task to be executed
+  /// @param logger          the logger on which to log if the execution of the task fails
+  /// @param level           the level of the log message
+  /// @param messageFunction a function to generate a log message given the `Throwable` raised by
+  ///                        the failed execution of the task
   public static void doOrLog(
       Runnable runnable,
       Logger logger,
@@ -82,17 +108,41 @@ public class Utils {
     }
   }
 
-  public static <E> List<E> fold(List<E> items, int fold, int n) {
-    return folds(items, List.of(fold), n);
+  /// Returns a list containing the items of the `fold`-indexed fold of the `n`-folding of the
+  /// provided `list`. Internally calls [#folds(List, List, int)].
+  ///
+  /// @param list the list to fold
+  /// @param fold the fold index to include
+  /// @param n    the total number of folds
+  /// @param <E>  the type of elements in the list
+  /// @return a new unmodifiable list containing only the items of the `fold`-th fold
+  public static <E> List<E> fold(List<E> list, int fold, int n) {
+    return folds(list, List.of(fold), n);
   }
 
-  public static <E> List<E> folds(List<E> items, List<Integer> folds, int n) {
-    return IntStream.range(0, items.size())
+  /// Returns a list containing the items in the `folds`-indexed folds of the `n`-folding of the
+  /// provided `list`. An `n`-folding is a list of approximately equal-sized disjoint sublists of
+  /// the list that, if concatenated, form the list itself. The folds are built using the modulo
+  /// operator: i.e., the third fold contains the items of the list whose index modulo `n` is 2.
+  ///
+  /// @param list  the list to fold
+  /// @param folds a list of fold indices to include
+  /// @param n     the total number of folds
+  /// @param <E>   the type of elements in the list
+  /// @return a new unmodifiable list containing only the items of the `folds`-indexed folds
+  public static <E> List<E> folds(List<E> list, List<Integer> folds, int n) {
+    return IntStream.range(0, list.size())
         .filter(i -> folds.contains(i % n))
-        .mapToObj(items::get)
+        .mapToObj(list::get)
         .toList();
   }
 
+  /// Returns a string representing the current user and machine name. The format is
+  /// "username@hostname". The method obtains the user through the `"user.name"` system property
+  /// (accessed through [System#getProperty(String)]) and the machine names through
+  /// [InetAddress#getHostName()] invoked on the local host address.
+  ///
+  /// @return a string identifying the user and machine
   public static String getUserMachineName() {
     String user = System.getProperty("user.name");
     String hostName = "unknown";
@@ -104,10 +154,28 @@ public class Utils {
     return user + "@" + hostName;
   }
 
-  public static <E> List<E> negatedFold(List<E> items, int fold, int n) {
-    return folds(items, IntStream.range(0, n).filter(j -> j != fold).boxed().toList(), n);
+  /// Returns a list containing the items not belonging to the `fold`-indexed fold of the
+  /// `n`-folding of the provided `list`. Internally calls [#folds(List, List, int)].
+  ///
+  /// @param list the list to fold
+  /// @param fold the fold index to include
+  /// @param n    the total number of folds
+  /// @param <E>  the type of elements in the list
+  /// @return a new unmodifiable list containing all but the items of the `fold`-th fold
+  public static <E> List<E> negatedFold(List<E> list, int fold, int n) {
+    return folds(list, IntStream.range(0, n).filter(j -> j != fold).boxed().toList(), n);
   }
 
+  /// Returns the `File` object representing the given `pathName`. If `overwrite` is `false` and a
+  /// file with the same name already exists, it generates a new unique file name by appending a
+  /// number or ".newer" suffix. The method creates the directories leading to the file if they do
+  /// not exist.
+  ///
+  /// @param pathName  the desired path for the file
+  /// @param overwrite if `false`, the returned file represents a renamed version of the given path;
+  ///                  otherwise, represents the given path
+  /// @return a `File` object representing the (potentially new) file path
+  /// @throws IOException if an I/O error occurs during directory creation or file path resolution
   public static File robustGetFile(String pathName, boolean overwrite) throws IOException {
     // create directory
     Path path = Path.of(pathName);
@@ -158,11 +226,24 @@ public class Utils {
     return dirsPath.resolve(filePath).toFile();
   }
 
-  public static void save(Object o, String filePath, boolean overwrite, boolean verbose) {
+  /// Saves an object to a file. The type of saving depends on the object type. A `BufferedImage` is
+  /// saved as a PNG image through [ImageIO#write(RenderedImage, String, File)]. A `String` is saved
+  /// as text. A `Binarizable` object is saved as raw binary data (obtained through
+  /// [Binarizable#data()]). A `byte[]` is saved as raw binary data. A `NamedParamMap` is saved as a
+  /// text, pretty-printed through [MapNamedParamMap#prettyToString()].
+  ///
+  /// @param object    the object to save
+  /// @param filePath  the path to the file where the object will be saved
+  /// @param overwrite if `true`, overwrites the file at `filePath`, if any; otherwise, obtain a new
+  ///                  path through [#robustGetFile(String, boolean)]
+  /// @param verbose   if `true`, prints a log message upon successful saving
+  /// @throws RuntimeException if an `IOException` occurs during saving, or if the object type is
+  ///                          not supported
+  public static void save(Object object, String filePath, boolean overwrite, boolean verbose) {
     File file = null;
     try {
       file = io.github.ericmedvet.jnb.datastructure.Utils.robustGetFile(filePath, overwrite);
-      switch (o) {
+      switch (object) {
         case BufferedImage image -> ImageIO.write(image, "png", file);
         case String s -> Files.writeString(
             file.toPath(),
@@ -192,7 +273,7 @@ public class Utils {
         );
         case null -> throw new IllegalArgumentException("Cannot save null data of type %s");
         default -> throw new IllegalArgumentException(
-            "Cannot save data of type %s".formatted(o.getClass().getSimpleName())
+            "Cannot save data of type %s".formatted(object.getClass().getSimpleName())
         );
       }
       if (verbose) {
@@ -206,6 +287,17 @@ public class Utils {
     }
   }
 
+  /// Returns a `Collector` that accumulates elements into a `SequencedMap`. The keys and values are
+  /// produced by applying the provided mapping functions to the input elements. If multiple input
+  /// elements map to the same key, the value from the first encountered element is retained. The
+  /// order of elements in the input stream is preserved in the resulting map.
+  ///
+  /// @param keyMapper   a function to extract the key from an input element
+  /// @param valueMapper a function to extract the value from an input element
+  /// @param <T>         the type of the input elements
+  /// @param <K>         the type of the keys in the map
+  /// @param <U>         the type of the values in the map
+  /// @return a `Collector` that accumulates elements into a `SequencedMap`
   public static <T, K, U> Collector<T, ?, SequencedMap<K, U>> toSequencedMap(
       Function<? super T, ? extends K> keyMapper,
       Function<? super T, ? extends U> valueMapper
@@ -218,6 +310,14 @@ public class Utils {
     );
   }
 
+  /// Returns a `Collector` that accumulates elements into a `SequencedMap`, where the input elements
+  /// themselves are the keys. The values are produced by applying the provided mapping function to
+  /// the input elements. Internally calls [#toSequencedMap(Function, Function)] with the identity as the first argument.
+  ///
+  /// @param valueMapper a function to extract the value from an input element
+  /// @param <T>         the type of the input elements (and keys in the map)
+  /// @param <U>         the type of the values in the map
+  /// @return a `Collector` that accumulates elements into a `SequencedMap`
   public static <T, U> Collector<T, ?, SequencedMap<T, U>> toSequencedMap(
       Function<? super T, ? extends U> valueMapper
   ) {
